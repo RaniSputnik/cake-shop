257690a983b3c21c82b26e57b5b624b60296e3e9
Ryan Loader <ryan.loader@cazoo.co.uk>
2022-05-02 09:40:43 +0100

    No baking over a weekend

--- src/main/kotlin/Order.kt
+++ src/main/kotlin/Order.kt
@@ -1,3 +1,4 @@
+import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -5,7 +6,15 @@ class Order(private val cake: Cake, private val orderDate: LocalDateTime) {
 
     fun getDeliveryDate(): LocalDate {
         val orderedOn = LocalDate.from(this.orderDate)
-        val leadTime = cake.bakingTime() + cake.decoratingTime()
+
+        val bakingTime = cake.bakingTime()
+        val decoratingTime = cake.decoratingTime()
+
+        val day = orderedOn.dayOfWeek.value
+        val daysUntilWeekend = DayOfWeek.SATURDAY.value - day
+        val idleBakingDays = if (bakingTime >= daysUntilWeekend) 2 else 0
+
+        val leadTime = bakingTime + idleBakingDays + decoratingTime
         return orderedOn.plusDays(leadTime.toLong())
     }
 }
--- src/test/kotlin/OrderTest.kt
+++ src/test/kotlin/OrderTest.kt
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Nested
 import java.time.LocalDate
 import java.time.LocalDateTime
 import kotlin.test.Test
@@ -12,30 +13,55 @@ internal class OrderTest {
         return LocalDate.parse(date)
     }
 
-    @Test
-    fun testSmallCakesHaveALeadTimeOfTwoDays() {
-        val smallCakeOrder = Order(Cake.small(), afternoon("2022-03-01"))
-        assertEquals(expect("2022-03-03"), smallCakeOrder.getDeliveryDate())
+    @Nested
+    inner class ProductLeadTimes {
 
-        val anotherOrder = Order(Cake.small(), afternoon("2022-05-04"))
-        assertEquals(expect("2022-05-06"), anotherOrder.getDeliveryDate())
+        @Test
+        fun testSmallCakesHaveALeadTimeOfTwoDays() {
+            val smallCakeOrder = Order(Cake.small(), afternoon("2022-03-01"))
+            assertEquals(expect("2022-03-03"), smallCakeOrder.getDeliveryDate())
+
+            val anotherOrder = Order(Cake.small(), afternoon("2022-05-04"))
+            assertEquals(expect("2022-05-06"), anotherOrder.getDeliveryDate())
+        }
+
+        @Test
+        fun testBigCakesHaveALeadTimeOfThreeDays() {
+            val smallCakeOrder = Order(Cake.big(), afternoon("2022-03-01"))
+            assertEquals(expect("2022-03-04"), smallCakeOrder.getDeliveryDate())
+
+            val anotherOrder = Order(Cake.big(), afternoon("2022-05-03"))
+            assertEquals(expect("2022-05-06"), anotherOrder.getDeliveryDate())
+        }
+
+        @Test
+        fun testCustomFrostingAddsTwoDaysLeadTime() {
+            val smallCakeOrder = Order(Cake.small().withCustomFrosting(), afternoon("2022-03-01"))
+            assertEquals(expect("2022-03-05"), smallCakeOrder.getDeliveryDate())
+
+            val anotherOrder = Order(Cake.big().withCustomFrosting(), afternoon("2022-03-07"))
+            assertEquals(expect("2022-03-12"), anotherOrder.getDeliveryDate())
+        }
     }
 
-    @Test
-    fun testBigCakesHaveALeadTimeOfThreeDays() {
-        val smallCakeOrder = Order(Cake.big(), afternoon("2022-03-01"))
-        assertEquals(expect("2022-03-04"), smallCakeOrder.getDeliveryDate())
+    @Nested
+    inner class NoBakingOverWeekends {
+        @Test
+        fun testACakeOrderedOnFridayStartsProductionOnMonday() {
+            val fridayOrder = Order(Cake.small(), afternoon("2022-03-04"))
+            assertEquals(expect("2022-03-08"), fridayOrder.getDeliveryDate())
+        }
 
-        val anotherOrder = Order(Cake.big(), afternoon("2022-05-03"))
-        assertEquals(expect("2022-05-06"), anotherOrder.getDeliveryDate())
-    }
+        @Test
+        fun testASmallCakeOrderedOnThursdayIsStartedOnFridayAndFinishedOnMonday() {
+            val thursdayOrder = Order(Cake.small(), afternoon("2022-03-03"))
+            assertEquals(expect("2022-03-07"), thursdayOrder.getDeliveryDate())
+        }
 
-    @Test
-    fun testCustomFrostingAddsTwoDaysLeadTime() {
-        val smallCakeOrder = Order(Cake.small().withCustomFrosting(), afternoon("2022-03-01"))
-        assertEquals(expect("2022-03-05"), smallCakeOrder.getDeliveryDate())
-
-        val anotherOrder = Order(Cake.big().withCustomFrosting(), afternoon("2022-03-07"))
-        assertEquals(expect("2022-03-12"), anotherOrder.getDeliveryDate())
+        @Test
+        fun testABigCakeOrderedOnWednesdayIsFinishedTheFollowingWeek() {
+            val thursdayOrder = Order(Cake.big(), afternoon("2022-03-09"))
+            assertEquals(expect("2022-03-14"), thursdayOrder.getDeliveryDate())
+        }
     }
 }
